70f25b1a561c4bdac0f1593c9655596a880e126d
bsu-jacksonmorton <70181389+bsu-jacksonmorton@users.noreply.github.com>
2022-09-25 17:33:02 -0600

    Implemented addPermission signature (#58)

--- webserver/src/api/drive-permission-manager/index.ts
+++ webserver/src/api/drive-permission-manager/index.ts
@@ -1,5 +1,5 @@
 import { GoogleAuth, OAuth2Client } from 'google-auth-library';
-import {File, Permission} from './types';
+import {File, GranteeType, Permission, Role} from './types';
 interface IDrivePermissionManager{
     client: OAuth2Client // Imports needed to read type.
     /**
@@ -21,5 +21,15 @@ interface IDrivePermissionManager{
      * @param permissionId the id of the permission to remove
      */
     deletePermission: (fileId: string, permissionId: string) => void
+    /**
+     * Adds a permission to the given File. If the GranteeType is user or group, then
+     * an email address will need to be passed. If the GranteeType is domain, then a
+     * domain must be provided.
+     * @param file the file to add the permission to
+     * @param role the role that should be granted by this permission
+     * @param type the GranteeType
+     * @param s the domain or email address for the domain, user, or group that the permission is for.
+     */
+    addPermission: (file: File, role: Role, type: GranteeType, s?: string) => void
 }
 
