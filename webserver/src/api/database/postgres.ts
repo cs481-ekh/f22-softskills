264f70d7e789abf66c962e053be525b72e3258a8
phillipbruce1 <phillipbruce@u.boisestate.edu>
2022-10-07 14:24:46 -0600

    Implemented #117, #118, #119, #120, closing #74

--- webserver/src/api/database/postgres.ts
+++ webserver/src/api/database/postgres.ts
@@ -1,101 +1,93 @@
 import { Pool } from 'pg';
+import { Users } from './users';
 
-const pool = new Pool({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE
-});
+export class Postgres {
+    private pool: Pool;
+    users: Users;
 
-// Initialize database tables and types
-async function init() {
-    /**
-     * User table
-     * 
-     * Columns:
-     * - EMAIL = String email of user - is also primary key
-     * - DISPLAY_NAME = Name of user (not null)
-     */
-    await pool.query("CREATE TABLE IF NOT EXISTS Users (EMAIL TEXT PRIMARY KEY NOT NULL, "
-        + "DISPLAY_NAME TEXT NOT NULL);", (err, res) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log(res);
-            }
+    constructor() {
+        this.pool = new Pool({
+            host: process.env.POSTGRES_HOST,
+            port: process.env.POSTGRES_PORT,
+            user: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DATABASE
         });
+        // this.initDatabases();
+        this.users = new Users(this.pool);
+    }
+
+    close() {
+        this.users.close();
+    }
 
     /**
-     * File table
-     * See: https://developers.google.com/drive/api/v3/reference/files
-     * 
-     * Columns:
-     * - ID = String ID of file - is also primary key of entry
-     * - KIND = Type of file
-     * - NAME = Name of file
-     * - PARENTS = String array of parent directory(s) by ID
-     * - CHILDREN = String array of children files by their ID (can be null)
-     * - OWNERS = String array of file owner(s) by their ID (can be null in shared Drive)
-     * - PERMISSIONS = String array of permissions assigned to file by their ID
+     * Initializes databases
      */
-    await pool.query("CREATE TABLE IF NOT EXISTS Files (ID TEXT PRIMARY KEY NOT NULL, "
-        + "KIND TEXT NOT NULL, NAME TEXT, PARENTS TEXT[] NOT NULL, CHILDREN TEXT[], "
-        + "OWNERS TEXT[], PERMISSIONS TEXT[]);", (err, res) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log(res);
-            }
-        });
-    /**
-     * Permission table
-     * See:
-     * https://developers.google.com/drive/api/v3/reference/permissions
-     * 
-     * Columns:
-     * - ID = String ID of permission - is also primary key of entry
-     * - EMAIL = Email of user or group who has access
-     * - TYPE = Type of grantee
-     * - ROLE = Role granted in permission
-     * - EXPIRATION_DATE = Date the access will expire
-     * - DELETED = Boolean as to whether the file has been deleted
-     * - PENDING_OWNER = Boolean as to whether the account is a pending owner
-     * - GRANTEE_USER = ID of user who has access
-     */
-    await pool.query("DO $$ BEGIN CREATE TYPE PERMISSION_ROLE AS ENUM "
-        + "('owner', 'organizer', 'fileOrganizer', 'writer', "
-        + "'commenter', 'reader'); EXCEPTION WHEN DUPLICATE_OBJECT THEN NULL; END $$",
-        async (err, res) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(res);
-                await pool.query("DO $$ BEGIN CREATE TYPE GRANTEE_TYPE AS ENUM "
-                    + "('user', 'group', 'domain', 'anyone'); EXCEPTION WHEN "
-                    + "DUPLICATE_OBJECT THEN NULL; END $$", async (err, res) => {
-                        if (err) {
-                            console.error(err);
-                        } else {
-                            console.log(res);
-                            await pool.query("CREATE TABLE IF NOT EXISTS Permissions "
-                                + "(ID TEXT PRIMARY KEY NOT NULL, EMAIL TEXT, TYPE GRANTEE_TYPE NOT NULL, "
-                                + "ROLE PERMISSION_ROLE, EXPIRATION_DATE TEXT, DELETED BOOLEAN NOT NULL, "
-                                + "PENDING_OWNER BOOLEAN, GRANTEE_USER TEXT NOT NULL);", (err, res) => {
-                                    if (err) {
-                                        console.error(err);
-                                    } else {
-                                        console.log(res);
-                                    }
-                                    pool.end();
-                                });
-                        }
-                    })
-            }
-        });
+    private async initDatabases() {
+        console.log("Init");
+    
+        /**
+         * File table
+         * See: https://developers.google.com/drive/api/v3/reference/files
+         * 
+         * Columns:
+         * - ID = String ID of file - is also primary key of entry
+         * - KIND = Type of file
+         * - NAME = Name of file
+         * - PARENTS = String array of parent directory(s) by ID
+         * - CHILDREN = String array of children files by their ID (can be null)
+         * - OWNERS = String array of file owner(s) by their ID (can be null in shared Drive)
+         * - PERMISSIONS = String array of permissions assigned to file by their ID
+         */
+        await this.pool.query("CREATE TABLE IF NOT EXISTS Files (ID TEXT PRIMARY KEY NOT NULL, "
+            + "KIND TEXT NOT NULL, NAME TEXT, PARENTS TEXT[] NOT NULL, CHILDREN TEXT[], "
+            + "OWNERS TEXT[], PERMISSIONS TEXT[]);", err => {
+                if (err)
+                    console.error(err);
+            });
+        /**
+         * Permission table
+         * See:
+         * https://developers.google.com/drive/api/v3/reference/permissions
+         * 
+         * Columns:
+         * - ID = String ID of permission - is also primary key of entry
+         * - EMAIL = Email of user or group who has access
+         * - TYPE = Type of grantee
+         * - ROLE = Role granted in permission
+         * - EXPIRATION_DATE = Date the access will expire
+         * - DELETED = Boolean as to whether the file has been deleted
+         * - PENDING_OWNER = Boolean as to whether the account is a pending owner
+         * - GRANTEE_USER = ID of user who has access
+         */
+        await this.pool.query("DO $$ BEGIN CREATE TYPE PERMISSION_ROLE AS ENUM "
+            + "('owner', 'organizer', 'fileOrganizer', 'writer', "
+            + "'commenter', 'reader'); EXCEPTION WHEN DUPLICATE_OBJECT THEN NULL; END $$",
+            async err => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    await this.pool.query("DO $$ BEGIN CREATE TYPE GRANTEE_TYPE AS ENUM "
+                        + "('user', 'group', 'domain', 'anyone'); EXCEPTION WHEN "
+                        + "DUPLICATE_OBJECT THEN NULL; END $$", async err => {
+                            if (err) {
+                                console.error(err);
+                            } else {
+                                await this.pool.query("CREATE TABLE IF NOT EXISTS Permissions "
+                                    + "(ID TEXT PRIMARY KEY NOT NULL, EMAIL TEXT, TYPE GRANTEE_TYPE NOT NULL, "
+                                    + "ROLE PERMISSION_ROLE, EXPIRATION_DATE TEXT, DELETED BOOLEAN NOT NULL, "
+                                    + "PENDING_OWNER BOOLEAN, GRANTEE_USER TEXT NOT NULL);", err => {
+                                        if (err)
+                                            console.error(err);
+                                    });
+                            }
+                        })
+                }
+            });
+        console.log("Init end");
+    }
 }
-
-init();
 
 // exmaples/notes
 
---
+++ webserver/src/api/database/users.ts
@@ -0,0 +1,122 @@
+import { Pool } from 'pg';
+import { User } from '../drive-permission-manager/types';
+
+export class Users {
+    private pool: Pool;
+
+    constructor(pool: Pool) {
+        this.pool = pool;
+        this.initDB();
+    }
+
+    /**
+     * Initializes the user table if it does not exist
+     * 
+     * Columns:
+     * - EMAIL = String email of user - is also primary key
+     * - DISPLAY_NAME = Name of user (not null)
+     */
+    private async initDB() {
+        await this.pool.query("CREATE TABLE IF NOT EXISTS Users (EMAIL TEXT PRIMARY KEY NOT NULL, "
+            + "DISPLAY_NAME TEXT NOT NULL);", err => {
+                if (err)
+                    console.error(err);
+            });
+    }
+
+    /**
+     * Creates the provided user entry in the database. Returns the
+     * user object created in the database. If undefined, then query
+     * was unsuccessful.
+     * 
+     * @param user - User object to create in database
+     * @returns - User object created in database
+     */
+    async create(user: User, callback?: Function): Promise<User | undefined> {
+        let userOut: User | undefined;
+        await this.pool.query("INSERT INTO Users (EMAIL, DISPLAY_NAME) "
+            + "VALUES ('" + user.emailAddress + "', '" + user.displayName + "');", err => {
+                if (err)
+                    console.error(err);
+                else
+                    userOut = user;
+                if (callback)
+                    callback(userOut);
+            });
+        return userOut;
+    }
+
+    /**
+     * Reads the user entry with the desired email
+     * 
+     * @param email - Email of user to find
+     * @returns - Desired user object, or undefined if not found
+     */
+    async read(email: String, callback?: Function): Promise<User | undefined> {
+        let user: User | undefined;
+        await this.pool.query("SELECT * FROM Users WHERE EMAIL LIKE '" + email + "';", (err, res) => {
+            if (err)
+                console.error(err);
+            else
+                user = {
+                    emailAddress: res.rows[0].email,
+                    displayName: res.rows[0].display_name
+                };
+            if (callback)
+                callback(user, err);
+        });
+        return user;
+    }
+
+    /**
+     * Stores the provided updated user object in the database. Returns
+     * the updated object, or undefined if unsuccessful.
+     * 
+     * @param user - Updated user object to store in the database
+     * @param callback - Optional callback function
+     * @returns - Updated user object
+     */
+    async update(user: User, callback?: Function): Promise<User | undefined> {
+        let userOut: User | undefined;
+        await this.pool.query("UPDATE Users SET EMAIL = '" + user.emailAddress
+            + "', DISPLAY_NAME = '" + user.displayName + "';", err => {
+                if (err)
+                    console.error(err);
+                else
+                    userOut = user;
+                if (callback)
+                    callback(userOut);
+            });
+        return userOut;
+    }
+
+    /**
+     * Deletes the desired user from the database and returns the correscponding User object
+     * 
+     * @param email - Email of user to be deleted
+     * @param callback - Callback function to execute after
+     * @returns - Deleted User object or undefined if unsuccessful
+     */
+    async delete(email: String, callback?: Function): Promise<User | undefined> {
+        let user: User | undefined;
+        console.log(email);
+        await this.pool.query("DELETE FROM Users WHERE EMAIL LIKE '" + email + "';", (err, res) => {
+            console.error(err);
+            console.log(res);
+            if (err)
+                console.error(err);
+            else
+                user = res.rows[0];
+            if (callback)
+                callback(user, err);
+        })
+        return user;
+    }
+
+    /**
+     * Closes the pool
+     */
+    close(): void {
+        this.pool.end();
+    }
+}
