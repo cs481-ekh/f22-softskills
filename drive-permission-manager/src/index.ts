2bd0cf280eef21920237fd8bbd097acd9405ddfd
bsu-jacksonmorton <jacksonmorton@u.boisestate.edu>
2022-10-09 23:41:33 -0600

    Work complete!

--- drive-permission-manager/src/index.ts
+++ drive-permission-manager/src/index.ts
@@ -1,7 +1,7 @@
 import { GoogleAuth, OAuth2Client } from 'google-auth-library';
 import {File, GranteeType, Permission, User, Role} from './types';
 import {drive_v3, google} from 'googleapis';
-
+import{Postgres} from '../../database/postgres'
 interface IDrivePermissionManager{
     /**
      * Returns an array of File objects.
@@ -36,8 +36,22 @@ interface IDrivePermissionManager{
 
 class DrivePermissionManager implements IDrivePermissionManager {
     private drive: drive_v3.Drive
+    private db: Postgres
     constructor(auth: GoogleAuth){
         this.drive = google.drive({version:"v3", auth});
+        this.db = new Postgres();
+        this.initDb(); // Will need to check to see if DB need
+    }
+    private async initDb(){
+        await this.db.initTables(); // Init db
+        const fileList: File[] = await this.getFiles(); // Get files from Google Drive
+        for(const file of fileList){
+          await this.db.files.create(file); // Create a file entry in DB
+          for(const permission of file.permissions){
+            await this.db.permissions.create(permission); // Create permission entry in DB
+            await this.db.users.create(permission.user); // Create user entry in DB
+          }
+        }
     }
     async getFiles(): Promise<File[]> {
         try{
