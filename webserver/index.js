245b29f8fb3a78dd089a05966c6e286df3848722
TheBardUnit <isaac.bard@gmail.com>
2022-10-14 11:14:32 -0600

    Got dummy data to load into landing page

--- webserver/index.js
+++ webserver/index.js
@@ -7,6 +7,11 @@ const fetch = require('node-fetch');
 // const auth = require("./routes/auth");
 // const login = require("./routes/login");
 
+const json = require('./output.json');
+
+
+
+
 
 app.use(session({
   resave: false,
@@ -18,7 +23,7 @@ app.use(session({
 // app.use("/auth", auth);
 // app.use("/login", login);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('pages/auth');
 });
 
@@ -35,65 +40,74 @@ app.use(passport.session());
 app.set('view engine', 'ejs');
 
 app.get('/success', async (req, res) => {
-    let fileList = await handleGetFiles(duhToken);
-    res.render('index', {array: fileList.files});
+  let fileList = await handleGetFiles(duhToken);
+  res.render('index', { array: fileList.files });
 
 });
 app.get('/error', (req, res) => res.send("error logging in"));
-app.get('/land', (req, res) => {res.render('land')});
 
-passport.serializeUser(function(user, cb) {
+app.get('/land', async (req, res) => {
+  res.render('land', { array: json });
+});
+
+
+
+
+
+
+passport.serializeUser(function (user, cb) {
   cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
+passport.deserializeUser(function (obj, cb) {
   cb(null, obj);
 });
 
 /*  Google AUTH  */
-const {authenticate} = require('@google-cloud/local-auth');
-const {google} = require('googleapis');
+const { authenticate } = require('@google-cloud/local-auth');
+const { google } = require('googleapis');
+const { response } = require('express');
 let duhToken;
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const GOOGLE_CLIENT_ID = '480000320991-3h6eq67pprqjk6so5m5ajmgvls8b5sbe.apps.googleusercontent.com';
 const GOOGLE_CLIENT_SECRET = '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';
 const GOOGLE_API_KEY = 'AIzaSyBBPLkIsanFxwBIKSwhi9bVGd9okmYFs4o';
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-      userProfile=profile;
-      duhToken = accessToken;
-      return done(null, userProfile);
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/callback"
+},
+  function (accessToken, refreshToken, profile, done) {
+    userProfile = profile;
+    duhToken = accessToken;
+    return done(null, userProfile);
   }
 ));
 
-async function handleGetFiles(accessToken){
-    const url = `https://www.googleapis.com/drive/v3/files?supportsAllDrives=true&includeItemsFromAllDrives=true&key=${GOOGLE_API_KEY}`;
-    const method = 'GET';
-    const headers = {
-        'Authorization': `Bearer ${accessToken}`,
-        "Accept": "application/json"
-    }
-    const request = await fetch(url, {method, headers})
-    const response = await request.json();
-    return response;
+async function handleGetFiles(accessToken) {
+  const url = `https://www.googleapis.com/drive/v3/files?supportsAllDrives=true&includeItemsFromAllDrives=true&key=${GOOGLE_API_KEY}`;
+  const method = 'GET';
+  const headers = {
+    'Authorization': `Bearer ${accessToken}`,
+    "Accept": "application/json"
+  }
+  const request = await fetch(url, { method, headers })
+  const response = await request.json();
+  return response;
 }
 app.get('/login', (req, res) => {
-    var name = 'test'
-    res.render('login', {name});
+  var name = 'test'
+  res.render('login', { name });
 })
 app.get('/auth/google',
-  passport.authenticate('google', { scope : ['profile', 'email', 'https://www.googleapis.com/auth/drive'] }));
+  passport.authenticate('google', { scope: ['profile', 'email', 'https://www.googleapis.com/auth/drive'] }));
 
 app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/error' }),
-  function(req, res) {
+  function (req, res) {
     // Successful authentication, redirect success.
     console.log(req.session.passport);
     res.redirect('/success');
   });
 
-app.listen(port , () => console.log('App listening on port ' + port));
+app.listen(port, () => console.log('App listening on port ' + port));
